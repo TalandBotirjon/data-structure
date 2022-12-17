6c6f0d280d78a82c8f19d009e92bf3aba305e1cc
TalandBotirjon <85695336+TalandBotirjon@users.noreply.github.com>
2022-12-17 21:49:08 +0500

    Two list convert (dict, list, tuble).

---
+++ twolist_convert.py
@@ -0,0 +1,10 @@
+list1 = 'abcde'
+list2 = [1, 2, 3, 4, 5]
+
+# list3 = [{x: y} for x in list1 for y in list2]  # dictionary
+# print(list3)
+# list3 = [[x,y] for x in list1 for y in list2]  # list
+# print(list3)
+
+# list3 = [(x,y) for x in list1 for y in list2]  # tuple
+# print(list3)
